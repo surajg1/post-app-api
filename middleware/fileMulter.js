7c811d00cd39331cdaa6184cbe854533090db5ae
Suraj R Gholap <47596504+surajg1@users.noreply.github.com>
2020-02-01 07:15:10 +0530

    heroku try3

--- middleware/fileMulter.js
+++ middleware/fileMulter.js
@@ -13,7 +13,7 @@ const storage = multer.diskStorage({
         if(isValid){
             error = null;
         }
-        cb(error, "backend-node/images");
+        cb(error, "backend/images");
     },
     filename: (req, file, cb)=>{
         const name = file.originalname.toLowerCase().split(' ').join('-');
